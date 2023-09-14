3afb9f17fdff36d92a296c100bd33c8b1ca5084c
Swaym Garg <swaym1420.be21@chitkara.edu.in>
2023-09-14 17:52:59 +0530

    all

---
+++ orders.js
@@ -0,0 +1,30 @@
+// let orders = [
+//     {
+//         productName:"Samsung Galaxy S23 Ultra",
+//         productNumber : "7599",
+//         paymentStatus : "Due",
+//         shipping : "Delivered",
+//     },
+    
+//     {
+//         productName:"Apple Iphone 13 Pro Max",
+//         productNumber : "6562",
+//         paymentStatus : "Paid",
+//         shipping : "Delivered",
+//     },
+    
+//     {
+//         productName:"Xiaomi 13 Pro",
+//         productNumber : "5214",
+//         paymentStatus : "Refunded",
+//         shipping : "Declined",
+//     },
+    
+//     {
+//         productName:"Google Pixel 5 pro",
+//         productNumber : "1532",
+//         paymentStatus : "Paid",
+//         shipping : "Pending",
+//     },
+    
+// ]
